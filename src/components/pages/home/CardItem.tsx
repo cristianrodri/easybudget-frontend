c0ca5fb5f8aa0ad718f359b63cca03172277f325
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-02 00:38:29 -0300

    card styles completed

--- src/components/pages/home/CardItem.tsx
+++ src/components/pages/home/CardItem.tsx
@@ -3,7 +3,9 @@ import {
   Card,
   CardHeader,
   CardContent,
-  Grid
+  Grid,
+  Box,
+  makeStyles
 } from '@material-ui/core'
 
 interface Props {
@@ -12,14 +14,50 @@ interface Props {
   avatar: JSX.Element
 }
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    borderRadius: '1rem',
+    padding: theme.spacing(2)
+  },
+  header: {
+    padding: theme.spacing(4),
+    '& *': {
+      margin: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  },
+  cardContent: {
+    paddingTop: 0
+  },
+  cardTitle: {
+    paddingBottom: theme.spacing(3)
+  }
+}))
+
 export const CardItem = ({ title, description, avatar }: Props) => {
+  const { card, header, cardContent, cardTitle } = useStyles()
   return (
-    <Grid item xs={12} sm={6} md={4}>
-      <Card>
-        <CardHeader avatar={avatar} />
-        <CardContent>
-          <Typography>{title}</Typography>
-          <Typography>{description}</Typography>
+    <Grid item xs={10} sm={5} md={3}>
+      <Card className={card}>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <CardHeader avatar={avatar} className={header} />
+        </Box>
+        <CardContent className={cardContent}>
+          <Typography
+            component="h3"
+            variant="h6"
+            align="center"
+            color="primary"
+            className={cardTitle}
+            gutterBottom
+          >
+            {title}
+          </Typography>
+          <Typography variant="body1" align="center" gutterBottom>
+            {description}
+          </Typography>
         </CardContent>
       </Card>
     </Grid>
