8cf1442968e15b4d248fa46378d9c52d7579ff41
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-04 12:40:39 -0300

    start styles signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,70 @@
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  makeStyles
+} from '@material-ui/core'
 import { Layout } from '@components/Layout'
 
-const signup = () => {
+const useStyles = makeStyles(theme => ({
+  containerMessage: {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: 16,
+    transform: 'scale(1.1)',
+    boxShadow: theme.shadows[10]
+  },
+  containerForm: {
+    backgroundColor: theme.palette.grey[300],
+    borderRadius: 16
+  }
+}))
+
+const Signup = () => {
+  const { containerMessage, containerForm } = useStyles()
   return (
     <Layout title="Signup" backgroundPage="signup">
-      <main>
-        <h1>signup</h1>
-      </main>
+      <Box
+        position="absolute"
+        height="100%"
+        zIndex="var(--before-header)"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flex="1"
+      >
+        <Box width="min(100%, 700px)" display="flex">
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="start"
+            className={containerMessage}
+            p={4}
+          >
+            <Typography component="h3" variant="h4" gutterBottom>
+              Easy Budget
+            </Typography>
+            <Typography>Manage all your incomes and expenses easily</Typography>
+          </Box>
+          <Box flex="1" p={4} borderRadius={16} className={containerForm}>
+            <Typography component="h3" variant="h4">
+              Sign up
+            </Typography>
+            <form>
+              <TextField label="Name" />
+              <TextField label="Email" />
+              <TextField label="Password" />
+              <Button type="submit" variant="contained" color="secondary">
+                sign up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Box>
     </Layout>
   )
 }
 
-export default signup
+export default Signup
