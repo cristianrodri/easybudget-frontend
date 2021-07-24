ab53059f9590bdcd80573f07661de896633086d9
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-24 03:48:41 -0300

    create dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@components/Layout'
+
+const Dashboard = () => {
+  return (
+    <Layout title="Dashboard">
+      <h1>Welcome auth user!</h1>
+    </Layout>
+  )
+}
+
+export default Dashboard
