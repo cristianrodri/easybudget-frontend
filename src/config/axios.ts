396d9247d131d09a2334cf468964166d3bcbe7e4
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-24 03:29:07 -0300

    add axios cliente and server isntance

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
-import { URL } from './url'
+import { CLIENT_URL, SERVER_URL } from './url'
 
-export const axiosInstance = axios.create({
-  baseURL: URL
+export const clientInstance = axios.create({
+  baseURL: CLIENT_URL,
+  timeout: 8000
+})
+
+export const serverInstance = axios.create({
+  baseURL: SERVER_URL
 })
