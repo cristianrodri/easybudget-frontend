e223c050897b1fca0d898d7932dd18f9d677a4cc
Cristian <contacto.cristianrodriguez@gmail.com>
2021-09-20 09:51:00 -0300

    testing homepage and header

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import { Header } from './Header'
 
-it('link has href="/" in header logo', () => {
-  render(<Header />)
-  const header = screen.getByTestId('homepage')
-  expect(header).toHaveAttribute('href', '/')
+describe('Header', () => {
+  beforeEach(() => {
+    render(<Header />)
+  })
+
+  it('link has href="/" in header logo', () => {
+    const header = screen.getByTestId('homepage')
+    expect(header).toHaveAttribute('href', '/')
+  })
 })
---
+++ src/test/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import IndexPage from '../pages/index'
+
+describe('IndexPage', () => {
+  beforeEach(() => {
+    render(<IndexPage />)
+  })
+
+  it('should render IndexPage', () => {
+    expect(screen.getByRole('main')).toBeInTheDocument()
+  })
+
+  it('should render Easy Budget as presentation title', () => {
+    expect(screen.getByText(/easy budget/i)).toBeInTheDocument()
+  })
+})
