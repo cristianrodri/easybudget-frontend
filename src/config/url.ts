c8c60967d079a4c0f296dcea4d68b3153860cd4e
Cristian <contacto.cristianrodriguez@gmail.com>
2021-10-25 21:46:33 -0300

    change server api env variable to next public

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,2 +1,2 @@
 export const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL
-export const SERVER_URL = process.env.SERVER_API_URL
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_API_URL
---
+++ src/custom-types/index.d.ts
@@ -0,0 +1,55 @@
+export interface Type {
+  jwt: string
+  user: User
+}
+
+export interface User {
+  id: number
+  username: string
+  email: string
+  provider: string
+  confirmed: boolean
+  blocked: null
+  role: number
+  created_at: string
+  updated_at: string
+  avatar: Avatar | null
+  categories: Category[]
+}
+
+export interface Formats {
+  thumbnail: Avatar
+  large: Avatar
+  medium: Avatar
+  small: Avatar
+}
+
+export interface Avatar {
+  id?: number
+  name: string
+  width: number
+  height: number
+  formats?: Formats
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  path?: null
+}
+
+export interface Category {
+  id: number
+  name: string
+  type: string
+  budgets: Budget[]
+  money: number
+}
+
+export interface Budget {
+  id: number
+  description: string
+  money: number
+  category: number
+  date: string
+}
