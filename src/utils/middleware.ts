4fce73e67a2cb3bf4ac1070eaa7d7ecb28fc3811
Cristian <contacto.cristianrodriguez@gmail.com>
2022-04-01 17:48:48 -0300

    make option function parameter into withAuthentication

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,9 +1,10 @@
 import { GetServerSideProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 
+// Function that will be called in getServerSideProps in PRIVATE pages to verify if the user is logged. If the NO auth user tries to browse private pages, it should be redirected to /login
 export const withAuthentication =
   <P, Q extends ParsedUrlQuery = ParsedUrlQuery>(
-    getServerSidePropsFn: GetServerSideProps<P, Q>
+    getServerSidePropsFn?: GetServerSideProps<P, Q>
   ): GetServerSideProps<P, Q> =>
   async ctx => {
     const token = ctx.req.cookies?.token
@@ -20,6 +21,7 @@ export const withAuthentication =
     return getServerSidePropsFn(ctx)
   }
 
+// Function that will be called in getServerSideProps in PUBLIC pages to verify if the user is logged. If the auth user tries to browse public pages (such as login or signup), it should be redirected to /dashboard
 export const withPublic = (): GetServerSideProps => async ctx => {
   const token = ctx.req.cookies?.token
 
