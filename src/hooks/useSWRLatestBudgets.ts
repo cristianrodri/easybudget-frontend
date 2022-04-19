5ad0efb8849a7be45a32d7c2eaf7968a6e29ea53
Cristian <contacto.cristianrodriguez@gmail.com>
2022-04-18 20:26:06 -0400

    add a constant to limit budgets

--- src/hooks/useSWRLatestBudgets.ts
+++ src/hooks/useSWRLatestBudgets.ts
@@ -11,14 +11,16 @@ const fetcher = (url: string) =>
   axios.get<ApiResponse>(url).then(res => res.data.data)
 
 export const useSWRLatestBudgets = () => {
+  const LIMIT_BUDGETS = 5
+
   const { data, mutate } = useSWR(
-    '/api/budget/get?_sort=date:DESC&_limit=5&_categorydata=true',
+    `/api/budget/get?_sort=date:DESC&_limit=${LIMIT_BUDGETS}&_categorydata=true`,
     fetcher
   )
 
   // This function will be called after a budget is created in post API
   const mutateByAddingNewBudget = (newBudget: Budget) => {
-    const updatedBudgets = [newBudget, ...data.slice(0, 4)]
+    const updatedBudgets = [newBudget, ...data.slice(0, LIMIT_BUDGETS - 1)]
 
     mutate(updatedBudgets, false)
   }
