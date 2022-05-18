74064789a6a571a2e81c637d076c8543fbf60a48
Cristian <contacto.cristianrodriguez@gmail.com>
2022-05-17 21:28:39 -0400

    display menu by adding a Button instead of popup component

--- src/components/common/AuthMenu.tsx
+++ src/components/common/AuthMenu.tsx
@@ -1,6 +1,5 @@
-import { useContext } from 'react'
-import { Avatar, Menu, MenuItem } from '@mui/material'
-import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state'
+import { MouseEvent, useContext, useState } from 'react'
+import { Avatar, Menu, MenuItem, Button } from '@mui/material'
 import { useRouter } from 'next/router'
 import { SERVER_URL } from '@config/url'
 import { getAvatarThumbnail } from '@utils/avatar'
@@ -15,6 +14,15 @@ export const AuthMenu = () => {
   const avatarUrl = getAvatarThumbnail(data)
   const router = useRouter()
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   const logout = async () => {
     const res = await clientGetApi('api/logout')
 
@@ -28,24 +36,36 @@ export const AuthMenu = () => {
   if (!data) return null
 
   return (
-    <PopupState variant="popover" popupId="demo-popup-menu">
-      {popupState => (
-        <>
-          <Avatar
-            style={{ cursor: 'pointer' }}
-            src={`${SERVER_URL}${avatarUrl}`}
-            title={data?.username}
-            {...bindTrigger(popupState)}
-          />
-          <Menu {...bindMenu(popupState)}>
-            <MenuItem onClick={() => router.push('/profile')}>Profile</MenuItem>
-            <MenuItem onClick={() => router.push('/categories')}>
-              Categories
-            </MenuItem>
-            <MenuItem onClick={logout}>Logout</MenuItem>
-          </Menu>
-        </>
-      )}
-    </PopupState>
+    <>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{ p: 0, minWidth: 0, '& span': { display: 'none' } }}
+      >
+        <Avatar
+          style={{ cursor: 'pointer' }}
+          src={`${SERVER_URL}${avatarUrl}`}
+          title={data?.username}
+        />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        <MenuItem onClick={() => router.push('/profile')}>Profile</MenuItem>
+        <MenuItem onClick={() => router.push('/categories')}>
+          Categories
+        </MenuItem>
+        <MenuItem onClick={logout}>Logout</MenuItem>
+      </Menu>
+    </>
   )
 }
