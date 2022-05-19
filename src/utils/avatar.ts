40a018e57d2f1a72d86a1ee28707b3c6031e7fa7
Cristian <contacto.cristianrodriguez@gmail.com>
2022-05-18 21:07:54 -0400

    get the correct url avatar in getAvatarThumbnail function

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -2,7 +2,5 @@ import { User } from '@custom-types'
 
 export const getAvatarThumbnail = (user: User) =>
   user?.avatar
-    ? user.avatar?.formats
-      ? user.avatar.formats.thumbnail.url
-      : user.avatar.url
+    ? user.avatar?.formats?.thumbnail?.url ?? user.avatar.url
     : user?.username.charAt(0).toUpperCase()
