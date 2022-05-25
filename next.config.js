0cbd05f323dfd7cb740c5996a422f200351dd9b7
Cristian <contacto.cristianrodriguez@gmail.com>
2022-05-24 21:29:29 -0400

    add domains values to next.config with production server url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_API_URL
+
+const domains = []
+
+domains.push(SERVER_URL.slice(SERVER_URL.lastIndexOf('/') + 1))
+
+if (process.env.NODE_ENV !== 'production') domains.push('localhost')
+
 module.exports = {
   images: {
-    domains: ['localhost']
+    domains
   }
 }
