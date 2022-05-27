4cc50d78b1dbce219beb8af88f5398a3d8a7ce9a
Cristian <contacto.cristianrodriguez@gmail.com>
2022-05-26 20:21:26 -0400

    add server axios timeout to 30s

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_API_URL
-
 const domains = []
 
-domains.push(SERVER_URL.slice(SERVER_URL.lastIndexOf('/') + 1))
+domains.push('easybudget-backend.herokuapp.com')
 
 if (process.env.NODE_ENV !== 'production') domains.push('localhost')
 
